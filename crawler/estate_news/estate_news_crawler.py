666a5a484f2b802e86143503efdf6ed3c8ba04af
seunghun_shinhan <rsh1502@naver.com>
2021-07-20 16:13:40 +0900

    0720 commit

--- crawler/estate_news/estate_news_crawler.py
+++ crawler/estate_news/estate_news_crawler.py
@@ -5,14 +5,17 @@ import datetime
 from bs4 import BeautifulSoup
 import requests
 import re
+import time
 import multiprocessing as mp
+import random
 from multiprocessing import freeze_support
 
 date_list = []
-today = datetime.datetime.today()
+today = datetime.date(2021,7,15)
 
-# 코드를 작성하고 있는 2021년 7월 15일 부터 2017년 1월 1일까지 date range.
-for num_day in range(1,1657):
+# 시간 차이를 구해서 기간의 날짜 수 만큼 range
+date_from_to = datetime.date(2021,7,15) - datetime.date(2012,1,1)
+for num_day in range(1,(date_from_to.days)+1):
     tmp_date = today - datetime.timedelta(days = num_day)
     tmp_date = str(tmp_date).split(' ')[0].replace('-','.')
     date_list.append(tmp_date)
@@ -25,21 +28,27 @@ header = {
 
 urls_list = []
 for day in date_list:
-    for num in [1,11,21,31,41]:
+    for num in [1,11,21,31]:
         urls_list.append(base_url.format(day,day,str(num)))
 
-link_list = []
-
 def get_naver_news_url(urll):
-    now_req    = requests.get(urll,headers=header).text
-    now_search = BeautifulSoup(now_req,'html.parser').find_all('div',class_='info_group')
+    link_dict = {}
+    link_list = []
+    
+    if requests.get(urll,headers=header).status_code == 404:
+        pass
+    else:
+        now_req    = requests.get(urll,headers=header)
+        now_search = BeautifulSoup(now_req.text,'html.parser').find_all('div',class_='info_group')
 
-    for parts in now_search:
-        if parts.find_all('a',href=re.compile('^https://news.naver.com')) == []:
-            pass
-        else:
-            link_found = parts.find_all('a',href = re.compile('^https://news.naver.com'))[0]['href']
-            link_list.append(link_found)
+        for parts in now_search:
+            if parts.find_all('a',href=re.compile('news.naver.com')) == []:
+                pass
+            else:
+                link_found = parts.find_all('a',href = re.compile('news.naver.com'))[0]['href']
+                link_list.append(link_found)
+            
+    time.sleep(random.uniform(1,3))
 
     return link_list
 
@@ -47,5 +56,7 @@ if __name__ == '__main__':
     freeze_support()
     pool = mp.Pool(mp.cpu_count())
     result = pool.map(get_naver_news_url,urls_list)
-    result_df = pd.concat(result,axis=0)
-    
+    result_df = pd.DataFrame(sum(result,[]))
+    result_df.columns = ['links']
+    result_df.to_csv('C:/Users/shic/Desktop/crawler_project/data_save/estate_news/article_links.csv',index=0)
+    # 9.39
--- crawler/estate_news/get_article_from_naver_news.py
+++ crawler/estate_news/get_article_from_naver_news.py
@@ -7,7 +7,7 @@ import requests
 import re
 import multiprocessing as mp
 
-links_df = pd.read_csv('C:/Users/shic/Desktop/crawler_project/data_save/estate_news/links_df.csv')
+links_df = pd.read_csv('C:/Users/shic/Desktop/crawler_project/data_save/estate_news/article_links.csv')
 
 base_url = 'https://search.naver.com/search.naver?where=news&query=%EB%B6%80%EB%8F%99%EC%82%B0114&sm=tab_opt&sort=0&photo=0&field=0&pd=3&ds={}&de={}&start={}'
 header = {
@@ -22,35 +22,38 @@ def estate_news_crawler(url_now):
     return_dict = {}
     soup = BeautifulSoup(requests.get(url_now,headers=header).text,'html.parser')
 
-    for soup_rm in soup.find_all('strong',class_='median_and_summary'):
-        soup_rm.decompose()
-
-    # 기사의 제목 가져오기
-    if soup.find_all('h3',id='articleTitle') == []:
-        return_dict['article_title'] = 'no article title'
-        #article_title.append('no article title')
+    if soup.status_code == 404:
+        pass
     else:
-        return_dict['article_title'] = soup.find_all('h3',id='articleTitle')[0].text
-        #article_title.append(soup.find_all('h3',id='articleTitle')[0].text)
+        for soup_rm in soup.find_all('strong',class_='median_and_summary'):
+            soup_rm.decompose()
 
-    # 기사의 본문 가져오기
-    if soup.find_all('div',id='articleBodyContents') == []:
-        return_dict['article_content'] = 'no article contents'
-        #article_contents.append('no article contents')
-    else:
-        return_dict['article_content'] = soup.find_all('div',id='articleBodyContents')[0].text
-        #article_contents.append(soup.find_all('div',id='articleBodyContents')[0].text)
+        # 기사의 제목 가져오기
+        if soup.find_all('h3',id='articleTitle') == []:
+            return_dict['article_title'] = 'no article title'
+            #article_title.append('no article title')
+        else:
+            return_dict['article_title'] = soup.find_all('h3',id='articleTitle')[0].text
+            #article_title.append(soup.find_all('h3',id='articleTitle')[0].text)
 
-    if soup.find_all('span',class_='t11') == []:
-        return_dict['article_author'] = 'no article authors'
-    else:
-        return_dict['article_author'] = soup.find_all('span',class_='t11')[0].text
-    
-    return return_dict
+        # 기사의 본문 가져오기
+        if soup.find_all('div',id='articleBodyContents') == []:
+            return_dict['article_content'] = 'no article contents'
+            #article_contents.append('no article contents')
+        else:
+            return_dict['article_content'] = soup.find_all('div',id='articleBodyContents')[0].text
+            #article_contents.append(soup.find_all('div',id='articleBodyContents')[0].text)
+
+        if soup.find_all('span',class_='t11') == []:
+            return_dict['article_author'] = 'no article authors'
+        else:
+            return_dict['article_author'] = soup.find_all('span',class_='t11')[0].text
+
+        return return_dict
 
 if __name__ == '__main__':
     pool = mp.Pool(mp.cpu_count())
     result = pool.map(estate_news_crawler,links_df['links'])
     result_df = pd.DataFrame(result)
-    result_df.to_csv('C:/Users/shic/Desktop/crawler_project/data_save/result_df.csv',index=0)
+    result_df.to_csv('C:/Users/shic/Desktop/crawler_project/data_save/article_df.csv',index=0)
 
