99f5b06b23e220ab8c052da871b8fe79779358dd
seunghun_shinhan <rsh1502@naver.com>
2021-07-26 16:40:37 +0900

    0726 going home

--- crawler/kb_land/kb_land_crawler_mp.py
+++ crawler/kb_land/kb_land_crawler_mp.py
@@ -17,11 +17,8 @@ header = {
     'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
 }
 
-crawler_shinhan = 'C:/Users/shic/Desktop/crawler_project/'
-crawler_seunghun = 'C:/Users/rsh15/Desktop/seunghuni/crawler_project/'
-
 # region code
-region_data = pd.read_csv(crawler_seunghun + 'data_save/kb_land/region_code.csv',
+region_data = pd.read_csv('C:/Users/shic/Desktop/crawler_project/data_save/kb_land/region_code.csv',
                           dtype = {'big_region_code' : str,
                                    'big_region_name' : str,
                                    'small_region_code' : str,
@@ -57,53 +54,60 @@ def kb_land_crawler(dates):
 
         # URL 접속
         tmp_req = requests.get(tmp_url, headers=header)
+        
+        if re.search("graphYn1",tmp_req.text) is None:
+            no_return_dataframe = pd.DataFrame({'년' : year,
+                                                '월' : month,
+                                                '시/도' : region[1]['big_region_name'],
+                                                '시/군/구': region[1]['small_region_name'],
+                                                '지수' : 0,
+                                                '증감률' : 0
+            })
+            df_list.append(no_return_dataframe)
+        
+        else:
+            ret_a = re.search("var graphYn1 = [$](.+?);", tmp_req.text, re.S).group(1)
+            ret_a = ret_a.replace(".trim('[", "").replace("') == '' ? false: true", "").replace('Date.UTC(', '')
 
-        ret_a = re.search("var graphYn1 = [$](.+?);", tmp_req.text, re.S).group(1)
-        ret_a = ret_a.replace(".trim('[", "").replace("') == '' ? false: true", "").replace('Date.UTC(', '')
+            date_list_jisu = []
+            data_list_jisu = []
 
-        date_list_jisu = []
-        data_list_jisu = []
+            jisu_split = ret_a.split('], [')
 
-        jisu_split = ret_a.split('], [')
-        
-        for jisu in jisu_split:
-            jisu_parser = jisu.split('), ')
-            
-            jisu_date_parser = jisu_parser[0].replace(' ','').split(',')
-            
-            date_list_jisu.append(','.join([jisu_date_parser[0],
-                                            str(int(jisu_date_parser[1]) + 1),
-                                            jisu_date_parser[2]]))
-            data_list_jisu.append(jisu_parser[1].replace(']','').replace(',',''))
-            
-        ret_b = re.search('var graphYn2 = [$](.+?);', tmp_req.text, re.S).group(1)
-        ret_b = ret_b.replace(".trim('[", "").replace("') == '' ? false: true", "").replace('Date.UTC(', '')
-        rate_split = ret_b.split('], [')
-        
-        date_list_rate = []
-        data_list_rate = []
-        
-        for rate in rate_split:
-            rate_parser = rate.split('), ')
-            
-            rate_date_parser = rate_parser[0].replace(' ','').split(',')
-            
-            date_list_rate.append(','.join([rate_date_parser[0],
-                                            str(int(rate_date_parser[1]) + 1),
-                                            rate_date_parser[2]]))
-            data_list_rate.append(rate_parser[1].replace(']','').replace(',',''))
-        
-        return_dataframe = pd.DataFrame({
-            '년'      : year,
-            '월'     : month,
-            '시/도'  : region[1]['big_region_name'],
-            '시/군/구': region[1]['small_region_name'],
-            #'date_list_jisu'   : date_list_jisu,
-            '지수'   : data_list_jisu,
-            #'date_list_rate'   : date_list_rate,
-            '증감률'   : data_list_rate
-        })
-        df_list.append(return_dataframe)
+            for jisu in jisu_split:
+                jisu_parser = jisu.split('), ')
+
+                jisu_date_parser = jisu_parser[0].replace(' ','').split(',')
+
+                date_list_jisu.append(jisu_parser[0])
+                data_list_jisu.append(jisu_parser[1].replace(']','').replace(',',''))
+
+            ret_b = re.search('var graphYn2 = [$](.+?);', tmp_req.text, re.S).group(1)
+            ret_b = ret_b.replace(".trim('[", "").replace("') == '' ? false: true", "").replace('Date.UTC(', '')
+            rate_split = ret_b.split('], [')
+
+            date_list_rate = []
+            data_list_rate = []
+
+            for rate in rate_split:
+                rate_parser = rate.split('), ')
+
+                rate_date_parser = rate_parser[0].replace(' ','').split(',')
+
+                date_list_rate.append(jisu_parser[0])
+                data_list_rate.append(rate_parser[1].replace(']','').replace(',',''))
+
+            return_dataframe = pd.DataFrame({
+                '년'      : year,
+                '월'     : month,
+                '시/도'  : region[1]['big_region_name'],
+                '시/군/구': region[1]['small_region_name'],
+                '지수_date'   : date_list_jisu,
+                '지수'   : data_list_jisu,
+                '증감률_date' : date_list_rate,
+                '증감률'   : data_list_rate
+            })
+            df_list.append(return_dataframe)
     
     return pd.concat(df_list,axis=0)
     
@@ -112,4 +116,4 @@ if __name__ == '__main__':
     pool = mp.Pool(mp.cpu_count())
     result = pool.map(kb_land_crawler,date_list[0:10])
     result_df = pd.concat(result,axis=0)
-    result_df.to_csv(crawler_seunghun + 'data_save/kb_land/kb_land_mp2.csv',index=0)
+    result_df.to_csv('C:/Users/shic/Desktop/crawler_project/data_save/kb_land/kb_land_mp3.csv',index=0)
