c09d6d8a280045d46f966d1789e38214ac3f0d4f
seunghunii <rsh1502@naver.com>
2021-08-08 23:35:28 +0900

    0808 estate news with press

--- main/estate_news/crawler/get_article_from_naver_news.py
+++ main/estate_news/crawler/get_article_from_naver_news.py
@@ -1,17 +1,14 @@
 import pandas as pd
-import numpy as np
-from selenium import webdriver
-import datetime
 from bs4 import BeautifulSoup
 import requests
 import re
 import multiprocessing as mp
 
-links_df = pd.read_csv('C:/Users/shic/Desktop/shinhan_simon/google_drive/crawler_data/estate_news/article_links.csv')
+links_df = pd.read_csv('C:/Users/rsh15/Google Drive/crawler_data/estate_news/article_links.csv')
 
 base_url = 'https://search.naver.com/search.naver?where=news&query=%EB%B6%80%EB%8F%99%EC%82%B0114&sm=tab_opt&sort=0&photo=0&field=0&pd=3&ds={}&de={}&start={}'
 header = {
-    'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+    'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36'
 }
 
 article_title    = []
@@ -41,14 +38,19 @@ def estate_news_crawler(url_now):
             return_dict['article_content'] = 'no article contents'
             #article_contents.append('no article contents')
         else:
-            return_dict['article_content'] = soup.find_all('div',id='articleBodyContents')[0].text
+            article_txt = soup.find_all('div',id='articleBodyContents')[0].text
+            return_dict['article_content'] = re.sub('\n|\t|\r','',article_txt)
             #article_contents.append(soup.find_all('div',id='articleBodyContents')[0].text)
 
         # 기사의 작성 언론사 가져오기
-        if soup.find_all('div',class_='article_header')[0].find_all('img',src=re.compile('.png$')) == []:
+        if soup.find_all('div',class_='press_logo') == []:
             return_dict['article_writer'] = 'no article writer'
+        elif soup.find_all('div',class_='press_logo')[0].find_all(
+                'img',src=re.compile('^https://mimgnews.pstatic.net/')) == []:
+            return_dict['article_writer'] = 'no articlewriter'
         else:
-            return_dict['article_writer'] = soup.find('div',class_='article_header').find('img',src=re.compile('.png$'))['title']
+            return_dict['article_writer'] = soup.find('div',class_='press_logo').find(
+                'img',src=re.compile('^https://mimgnews.pstatic.net/'))['title']
 
         # 기사의 작성 일자시간 가져오기
         if soup.find_all('span',class_='t11') == []:
@@ -62,5 +64,5 @@ if __name__ == '__main__':
     pool = mp.Pool(mp.cpu_count())
     result = pool.map(estate_news_crawler,links_df['links'])
     result_df = pd.DataFrame(result)
-    result_df.to_csv('C:/Users/shic/Desktop/shinhan_simon/google_drive/crawler_data/estate_news/article_df2.csv',index=0)
+    result_df.to_csv('C:/Users/rsh15/Google Drive/crawler_data/estate_news/article_df2.csv',index=0)
 
