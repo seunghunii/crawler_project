4c84eb82c69a671ed828d3d24cc718047bba9809
seunghunii <rsh1502@naver.com>
2021-08-20 00:02:50 +0900

    0820 prepare for code submit

--- submit/부동산114뉴스기사/1.get_naver_news_link.py
+++ submit/부동산114뉴스기사/1.get_naver_news_link.py
@@ -1,6 +1,4 @@
 import pandas as pd
-import numpy as np
-from selenium import webdriver
 import datetime
 from bs4 import BeautifulSoup
 import requests
@@ -13,8 +11,8 @@ from multiprocessing import freeze_support
 date_list = []
 today = datetime.date(2021,8,1)
 
-# 시간 차이를 구해서 기간의 날짜 수 만큼 range
-date_from_to = today - datetime.date(2021,7,14)
+# 시간 차이를 구해서 기간의 날짜 수 만큼 range.
+date_from_to = today - datetime.date(2012,1,1)
 for num_day in range(1,(date_from_to.days)+1):
     tmp_date = today - datetime.timedelta(days = num_day)
     tmp_date = str(tmp_date).split(' ')[0].replace('-','.')
@@ -26,23 +24,30 @@ header = {
     'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
 }
 
+# 날짜와 시작 페이지 번호를 조합해 최종 url 목록을 만듭니다.
 urls_list = []
 for day in date_list:
     for num in [1,11,21,31]:
         urls_list.append(base_url.format(day,day,str(num)))
 
+# 네이버뉴스에서 부동산114로 검색하여 나온 뉴스기사들 중, 네이버 뉴스의 링크만을 가져옵니다.
 def get_naver_news_url(urll):
     link_list = []
     
     if requests.get(urll,headers=header).status_code == 404:
         pass
+
     else:
         now_req    = requests.get(urll,headers=header)
         now_search = BeautifulSoup(now_req.text,'html.parser').find_all('div',class_='info_group')
 
+        # 검색결과에서 기본적인 정보를 가진 info_group을 파싱합니다.
         for parts in now_search:
+
+            # 파싱한 결과에 포함된 네이버 뉴스 링크를 가져옵니다.
             if parts.find_all('a',href=re.compile('news.naver.com')) == []:
                 pass
+
             else:
                 link_found = parts.find_all('a',href = re.compile('news.naver.com'))[0]['href']
                 link_list.append(link_found)
@@ -57,5 +62,5 @@ if __name__ == '__main__':
     result = pool.map(get_naver_news_url,urls_list)
     result_df = pd.DataFrame(sum(result,[]))
     result_df.columns = ['links']
-    result_df.to_csv('C:/Users/shic/Desktop/shinhan_simon/google_drive/crawler_data/estate_news/article_links_after715.csv',index=0)
+    result_df.to_csv('C:/Users/shic/Desktop/shinhan_simon/google_drive/crawler_data/estate_news/article_links.csv',index=0)
     # 9.39
--- submit/부동산114뉴스기사/2.get_article_content.py
+++ submit/부동산114뉴스기사/2.get_article_content.py
@@ -4,9 +4,8 @@ import requests
 import re
 import multiprocessing as mp
 
-links_df = pd.read_csv('C:/Users/shic/Desktop/shinhan_simon/google_drive/crawler_data/estate_news/article_links_after715.csv')
+links_df = pd.read_csv('C:/Users/shic/Desktop/shinhan_simon/google_drive/crawler_data/estate_news/article_links.csv')
 
-base_url = 'https://search.naver.com/search.naver?where=news&query=%EB%B6%80%EB%8F%99%EC%82%B0114&sm=tab_opt&sort=0&photo=0&field=0&pd=3&ds={}&de={}&start={}'
 header = {
     'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36'
 }
@@ -14,7 +13,8 @@ header = {
 article_title    = []
 article_contents = []
 
-def estate_news_crawler(url_now):
+# 네이버 뉴스 웹페이지에서 제목, 본문, 언론사, 작성일자를 가져오는 함수입니다.
+def get_article_data(url_now):
     
     return_dict = {}
     soup = BeautifulSoup(requests.get(url_now,headers=header).text,'html.parser')
@@ -28,19 +28,15 @@ def estate_news_crawler(url_now):
         # 기사의 제목 가져오기
         if soup.find_all('h3',id='articleTitle') == []:
             return_dict['article_title'] = 'no article title'
-            #article_title.append('no article title')
         else:
             return_dict['article_title'] = soup.find_all('h3',id='articleTitle')[0].text
-            #article_title.append(soup.find_all('h3',id='articleTitle')[0].text)
 
         # 기사의 본문 가져오기
         if soup.find_all('div',id='articleBodyContents') == []:
             return_dict['article_content'] = 'no article contents'
-            #article_contents.append('no article contents')
         else:
             article_txt = soup.find_all('div',id='articleBodyContents')[0].text
             return_dict['article_content'] = re.sub('\n|\t|\r','',article_txt)
-            #article_contents.append(soup.find_all('div',id='articleBodyContents')[0].text)
 
         # 기사의 작성 언론사 가져오기
         if soup.find_all('div',class_='press_logo') == []:
@@ -60,9 +56,10 @@ def estate_news_crawler(url_now):
 
         return return_dict
 
+# 병렬처리를 한 뒤, 결과를 합쳐 저장합니다.
 if __name__ == '__main__':
     pool = mp.Pool(mp.cpu_count())
-    result = pool.map(estate_news_crawler,links_df['links'])
+    result = pool.map(get_article_data,links_df['links'])
     result_df = pd.DataFrame(result)
-    result_df.to_csv('C:/Users/shic/Desktop/shinhan_simon/google_drive/crawler_data/estate_news/article_df_after715.csv',index=0)
+    result_df.to_csv('C:/Users/shic/Desktop/shinhan_simon/google_drive/crawler_data/estate_news/article_df.csv',index=0)
 
